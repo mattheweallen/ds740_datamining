18f01b9641e54c98a419775497f20060c552bd50
matt <mattheweallen@gmail.com>
2019-02-25 18:40:30 -0600

    notes for first lecture

--- week6/week6.R
+++ week6/week6.R
@@ -70,4 +70,57 @@ fit.w$coefficients
 
 #robust regression put less weight on outliers or influential points
 #weights decrease as absolute value of residuals increase
+#http://www.saedsayad.com/docs/RobustRegression.pdf
 
+library(MASS)
+plot(hills$time ~ hills$dist, cex.axis=1.2,las=1, ylab="Time", xlab="Distance")
+fit = lm(time ~ dist, data = hills)
+abline(fit, lwd=2)
+legend("topleft", legend="OLS", lty=1)
+
+par( mfrow = c(2, 2) )
+plot(fit)
+
+#while loop for tukey's bisquare method
+fit.w = lm(time ~ dist, data = hills)
+
+oldcoef = rep(0, length(fit.w$coef))
+newcoef = fit.w$coef
+iter = 0
+
+while(sum(abs(oldcoef-newcoef))>.0001 & iter < 100) {
+  MAR = median(abs(fit.w$residuals))
+  k = 4.685*MAR/0.6745
+  w = (pmax(1-(fit.w$residuals/k)^2,0))^2
+  fit.w = lm(time ~ dist, data = hills, weights = w)
+  
+  iter = iter + 1
+  oldcoef = newcoef
+  newcoef = fit.w$coefficients
+}
+newcoef
+fit.w$coefficients
+
+####
+library(MASS)
+fit.bisquare = rlm(time ~ dist, data = hills, psi = psi.bisquare)
+fit.huber = rlm(time ~ dist, data = hills, psi = psi.huber)
+
+par( mfrow = c(1, 1) )
+plot(hills$time ~ hills$dist, cex.axis=1.2,las=1, ylab="Time", xlab="Distance")
+abline(fit, lwd=2)
+abline(fit.bisquare, lwd=2, col="blue", lty=2)
+abline(fit.huber, lwd=2, col="red", lty=3)
+legend("topleft", legend=c("OLS", "Huber", "Bisquare"), lty=c(1,3,2), col=c("black","red","blue"))
+
+
+
+#install.packages("car")
+{
+library(car)
+plot(fit.bisquare$w, las=1, cex.axis=1.2, ylab="Weights")
+smallweights = which(fit.bisquare$w < .8)
+showLabels(1:dim(hills)[1], fit.bisquare$w, rownames(hills), id.method = smallweights)
+}
+
+#https://www.cs.umd.edu/~mount/Papers/lts-manuscript07.pdf
