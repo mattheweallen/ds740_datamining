9c7b1041fe5e128c7098edfa17b810bf5f38db81
matt <mattheweallen@gmail.com>
2019-03-03 15:05:41 -0600

    completed ww7

--- week7/ww7/ww7.r
+++ week7/ww7/ww7.r
@@ -6,3 +6,33 @@ plot(mytree)
 text(mytree, pretty = 0)
 
 summary(mytree)
+
+mytree
+
+#problem 5
+set.seed(400)
+cancer.cv = cv.tree(mytree, FUN = prune.misclass)
+cancer.cv
+
+prune.cancer = prune.misclass(mytree, best=3)
+plot(prune.cancer)
+text(prune.cancer, pretty=0)
+
+#problem 6
+library(gbm)
+set.seed(400)
+boost = gbm(Survival01~., data=cancer, distribution = "bernoulli",n.trees=5000, shrinkage = .001, interaction.depth=2)
+summary(boost)
+
+plot(boost, i="Nodes")
+  
+
+#problem 7
+library(randomForest)
+set.seed(400)
+cancer.bag = randomForest(as.factor(Survival01)~., data=cancer, mtry = 3, importance = T) 
+cancer.bag
+
+plot(cancer.bag)
+importance(cancer.bag)
+varImpPlot(cancer.bag)
