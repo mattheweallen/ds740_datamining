48e08d5d0a68f16722bb1d53409993f139bdd3cd
irgepi <matthew.allen@irco.com>
2019-03-25 10:15:02 -0500

    completed web work, added week 9 hw

--- week9/ww9/ww9.R
+++ week9/ww9/ww9.R
@@ -65,4 +65,65 @@ sqrt(1/2.25)
 #0.3535534
 
 #web work 9 Prob 2
-bank = read.csv("bank-additional.csv")
+bank = read.csv("bank-additional.csv", sep = ";")
+attach(bank)
+my.col = rep("red",4119)
+my.col[which(y=="yes")] = "blue" 
+my.pch = rep(21,4119)
+my.pch[which(y=="yes")] = 4
+
+plot(scale(emp.var.rate),scale(duration),col=my.col,pch=my.pch)
+legend("topleft", legend = c("No", "Yes"), pch = c(21, 4), col = c("red","blue"))
+
+library(e1071)
+svmfit = svm(y~emp.var.rate+duration, data = bank, kernel = "linear", cost = 1)
+
+summary(svmfit)
+#confusion matrix
+table(svmfit$fitted,y)
+16/(16+3652)
+417/(34+417)
+table(bank$y)
+
+
+library(e1071)
+svmfit = svm(y~emp.var.rate+duration, data = bank, kernel = "linear", cost = 1)
+
+w = colSums(svmfit$coefs[,1] * svmfit$SV)
+b = svmfit$rho
+# Optimal line
+abline(b/w[2],-w[1]/w[2]) # y-int, slope
+# Margin lines
+abline((b-1)/w[2],-w[1]/w[2], lty=2) # y-int, slope; 
+# as cost decreases, w[2] tends to decrease, resulting in a larger margin
+abline((b+1)/w[2],-w[1]/w[2], lty=2) 
+-w[1]/w[2]
+
+
+#problem 5
+set.seed(999)
+tune.out = tune(svm, y~emp.var.rate + duration, data = bank, kernel = "radial", 
+                ranges = list( cost = c(.001, .01, .1, 1, 5, 10, 100), 
+                               gamma = c(0.5, 1, 2, 3, 4) ))
+
+best.mod = tune.out$best.model
+summary(best.mod)
+
+summary(tune.out)
+summary(tune.out)$best.performance
+
+newClient = data.frame(emp.var.rate = 1, duration = 250)
+predict(best.mod, newdata = newClient)
+
+
+#problem 6
+xgrid = cbind(rep(seq(-4, 2, length=1000), 1000), 
+              rep(seq(0, 4000, length=1000), each=1000) )
+
+#problem 7
+func = predict(best.mod, xgrid, decision.values = TRUE)
+
+func = attributes(func)$decision
+#draw the curve f(x)=0
+contour(seq(-4,2,length=1000), seq(0,4000,length=1000),
+        matrix(func,1000,1000), level = 0, add=T, lwd=2)
