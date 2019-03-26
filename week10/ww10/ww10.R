38dae8a3a9276f31cbfe0c2f4546f211ae89e64e
matt <mattheweallen@gmail.com>
2019-03-26 07:28:24 -0500

    more web work 10

--- week10/ww10/ww10.R
+++ week10/ww10/ww10.R
@@ -1,3 +1,4 @@
+set.seed(20)
 x = seq(from = 0, to = 2*pi, by=.01)
 y = sin(x)
 library(nnet)
@@ -13,3 +14,11 @@ fit1 = nnet(y~x, size = 1, linout=T)
 fit2 = nnet(y~x, size = 2, linout=T)
 matplot(cbind(x,x,x), cbind(y, fit1$fitted.values, fit2$fitted.values), type="l", lwd=2, col=c("black", "blue", "red"))
 legend("topright", legend = c("y = sin(x)", "1 Hidden Node", "2 Hidden Nodes"), lty=c(1,2,3), col=c("black", "blue", "red"))
+
+fit0
+fit1$convergence
+fit2$convergence
+
+fit2 = nnet(y~x, size = 2, linout=T, maxit = 500)
+set.seed(16)
+fit2 = nnet(y~x, size = 2, linout=T, maxit = 500)
