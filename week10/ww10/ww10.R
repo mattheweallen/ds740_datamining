2d65c12921aa8f36d7e46be5e546fe17d002c425
matt <mattheweallen@gmail.com>
2019-03-26 07:27:18 -0500

    added more ww10

--- week10/ww10/ww10.R
+++ week10/ww10/ww10.R
@@ -6,3 +6,10 @@ fit0 = nnet(y~x, size = 0, linout = T, skip = T)
 
 matplot(cbind(x,x), cbind(y, fit0$fitted.values), type = "l")
 legend("topright", legend = c("y = sin(x)", "Estimated"), lty=c(1,2), col=c("black","red"))
+
+
+# do NOT reset the seed after running Problem 1
+fit1 = nnet(y~x, size = 1, linout=T)
+fit2 = nnet(y~x, size = 2, linout=T)
+matplot(cbind(x,x,x), cbind(y, fit1$fitted.values, fit2$fitted.values), type="l", lwd=2, col=c("black", "blue", "red"))
+legend("topright", legend = c("y = sin(x)", "1 Hidden Node", "2 Hidden Nodes"), lty=c(1,2,3), col=c("black", "blue", "red"))
